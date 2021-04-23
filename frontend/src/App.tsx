7c1ff7575255f33fd273da62810b9e1877f36d5c
Panos61 <panagiwtis.orovas@gmail.com>
2021-04-23 19:50:46 +0300

    Added Register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
 import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MenuBar from './components/MenuBar';
-//import { Dashboard, Login, Register, Welcome } from './pages/index';
+import { MainPage, Login, Register } from './pages/index';
 import { Container } from 'semantic-ui-react';
-import MainPage from './pages/MainPage';
-import Login from './pages/Login';
 
 function App() {
   return (
@@ -13,6 +11,7 @@ function App() {
       <Route exact path='/' component={MainPage} />
       <Container>
         <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
       </Container>
     </Router>
   );
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import '../pages/styles/login.style.css';
+import { Form, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+interface FormValues {
+  email: string;
+  username: string;
+  password: string;
+}
+
+interface Props {
+  onSubmit: (values: FormValues) => void;
+}
+
+const Register: React.FC<Props> = () => {
+  const [input, setInput] = useState({
+    email: '',
+    username: '',
+    password: '',
+  });
+
+  const { email, username, password } = input;
+
+  const onChange = (e: any) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: any) => {
+    console.log(input);
+    e.preventDefault();
+  };
+
+  return (
+    <div className='form-container' onSubmit={handleSubmit}>
+      <Form noValidate>
+        <h2>Create an account</h2>
+        <Form.Field>
+          <label>Email</label>
+          <input
+            type='text'
+            name='email'
+            placeholder='Email'
+            onChange={(e) => onChange(e)}
+            value={email}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Username</label>
+          <input
+            type='text'
+            name='username'
+            placeholder='Username'
+            onChange={(e) => onChange(e)}
+            value={username}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            onChange={(e) => onChange(e)}
+            value={password}
+          />
+        </Form.Field>
+        <p>
+          By signing up, you agree to the{' '}
+          <a href='/login' style={{ textDecoration: 'underline' }}>
+            Terms and Conditions
+          </a>
+        </p>
+
+        <Button type='submit' color='brown'>
+          Sign up
+        </Button>
+
+        <p>
+          Already have an account? <Link to='/login'>Login.</Link>
+        </p>
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
