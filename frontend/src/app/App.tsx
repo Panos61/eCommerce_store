7abcff07f472f656876e24b7e3bb14cc246aa1b0
Panos61 <panagiwtis.orovas@gmail.com>
2021-05-03 17:39:51 +0300

    hostory.push redirect

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,13 @@
 import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Router } from 'react-router';
 import { MainPage, Login, Register } from '../pages/index';
 import { Container } from 'semantic-ui-react';
+import history from '../common/utils/history';
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Route exact path='/' component={MainPage} />
       <Container>
         <Route exact path='/login' component={Login} />
