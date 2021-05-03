ab20e1605c3baae3d76520bd5ccee6e3b248f3e8
Panos61 <panagiwtis.orovas@gmail.com>
2021-05-03 17:39:03 +0300

    Set up redux store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,1 +1,18 @@
-import React from 'react';
+import { AuthState, authReducer } from '../features/auth/reducer';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+export interface RootState {
+  auth: AuthState;
+}
+
+export const store = createStore(
+  //   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+  //     (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(),
+
+  combineReducers({
+    auth: authReducer,
+  }),
+  composeWithDevTools(applyMiddleware(thunk))
+);
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
 import 'semantic-ui-css/semantic.min.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
 
 const client = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={client}>
-    <App />
-  </QueryClientProvider>,
+  <Provider store={store}>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </Provider>,
   document.getElementById('root')
 );
